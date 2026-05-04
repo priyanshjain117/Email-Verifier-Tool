b714d7bed04c52310f620731ede95f4df3a231b3
priyanshjain117 <jainpriyanshu117@gmail.com>
2026-05-04 22:45:13 +0530

    basic str

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import "fmt"
+import "regexp"
+
+func isValidFormat(email string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+	return re.MatchString(email)
+}
 
 func main(){
 	email:="example@gmail.com"
-	valid := VerifyEmail(email)
-	fmt.Println(valid)
+	valid := isValidFormat(email)
+	if valid {
+		fmt.Println("✅ Email is valid")
+	} else {
+		fmt.Println("❌ Email is invalid")
+	}
 	fmt.Println(email)
 }
